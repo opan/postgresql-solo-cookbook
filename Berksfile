51967d878cb114176ac1070cbe61b8fa9e159876
opanmustopah <opan.mustopah@go-jek.com>
2020-02-11 17:28:35 +0700

    Lock postgresql cookbook version to 7.1.5

--- Berksfile
+++ Berksfile
@@ -1,3 +1,4 @@
 source  'https://supermarket.chef.io'
 
+# cookbook 'postgresql', path: './cookbooks/postgresql'
 metadata
--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures postgresql_wrapper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
-depends 'postgresql'
+depends 'postgresql', '= 7.1.5'
 depends 'build-essential'
 depends 'apt'
